bcee6e20ce2177c61137b3479431010ca155a912
baibizhe <33809209+baibizhe@users.noreply.github.com>
2025-06-27 09:38:30 +0800

    Update run_install.sh

--- run_install.sh
+++ run_install.sh
@@ -1,8 +1,6 @@
-cd /inspire/hdd/project/robot-dna/sujiadi-p-sujiadi/baibizhe-tmp/r1-reasoning/CPPO
 pip install -e ".[dev]"
 cd ../trl
 pip install -e .
 cd ../KernelBench
 pip install -r requirements.txt
 pip install -e .
-cd /inspire/hdd/project/robot-dna/sujiadi-p-sujiadi/baibizhe-tmp/r1-reasoning/CPPO
